cb4bbfd0bb32012081acb7eaa5dc700401802bf4
Michael Sharp <msharp.oh@gmail.com>
2015-06-24 12:01:14 -0400

    Create ValidIPRegex.java

---
+++ ValidIPRegex.java
@@ -0,0 +1,31 @@
+
+/* This algorithm uses a regex pattern to find valid IP addresses from a list of string inputs
+    - by Michael Sharp
+    - msharp.oh@gmail.com
+    - www.softwareontheshore.com */
+    
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class ValidIPRegex {
+    static Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        while(in.hasNext()) {
+            String ip = in.next();
+            System.out.println(ip.matches(new myRegex().pattern));
+        }
+    }
+}
+
+public class myRegex {
+    String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
+
+  
+    
