c73a4855580157d893ab2e0f0cf461f4da7100bb
Michael Sharp <msharp.oh@gmail.com>
2015-06-24 12:19:56 -0400

    Create ReverseBigDecimal.java

---
+++ ReverseBigDecimal.java
@@ -0,0 +1,25 @@
+/* Sorts a BigDecimal Array in reverseOrder */
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Solution {
+    static Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        BigDecimal[] bigDecimalArray = new BigDecimal[n];
+        
+        for (int i = 0; i < n; i++) {
+            bigDecimalArray[i] = in.nextBigDecimal();
+         }
+        
+        Arrays.sort(bigDecimalArray, Collections.reverseOrder());
+        
+        for (BigDecimal big : bigDecimalArray) {
+            System.out.println(big);
+        }
+    }
+}
