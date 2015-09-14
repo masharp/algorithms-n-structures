1fb06c03f819202a6647f99a8189ac3cfa89d77b
Michael Sharp <msharp.oh@gmail.com>
2015-09-14 15:45:18 -0400

    added mergesort

--- Sort/MergeSort.js
+++ Sort/MergeSort.js
@@ -1,35 +1,33 @@
 /* This is a Merge Sort sorting algorithm that divides itself in half and calls itself until sorted and then merges
-the two halves. The below array of 20 unsorted elements are sorted in -90- iterations.
+the two halves. The below array of 20 unsorted elements are sorted in -67- iterations.
   - by Michael Sharp
   - msharp.oh@gmail.com
   - www.softwareontheshore.com */
 
 var numArray = [12, 122, 222, 4, 5, 9, 12323, 99, 10, 1, 78, 123, 64234, 6663, 99, 46, 2, 124, 18, 22];
-var arrayLength = numArray.length;
-var front = 0;
-var end = arrayLength - 1;
 var iterations = 0;
 
-mergeSort(numArray, front, end);
-
-
-for (var i = 0; i < arrayLength; i++) {
-  console.log(numArray[i]);
-}
-console.log(iterations + " " + arrayLength);
+console.log(mergeSort(numArray));
+console.log(iterations + " " + numArray.length);
 
 //-----------------------------------------
-function mergeSort(array, front, end) {
-  var pivot;
-  
-  if (end > front) {
-    pivot = Math.floor((front + right) / 2);
-    mergeSort(array, left, pivot);
-    mergeSort(array, pivot+1, end);
-    merge(array, end, pivot+1, end);
-  }
+function mergeSort(array) {
+
+  if (array.length < 2) return array;
+
+  var pivot = Math.floor(array.length / 2),
+      leftArray = mergeSort(array.slice(0, pivot)),
+      rightArray = mergeSort(array.slice(pivot, array.length));
+
+  return merge(leftArray, rightArray);
 }
 
-function merge(array, front, pivot, end) {
+function merge(leftArray, rightArray) {
+  var resultArray = [];
 
+  while(leftArray.length > 0 && rightArray.length > 0) {
+    resultArray.push(leftArray[0] < rightArray[0] ? leftArray.shift() : rightArray.shift());
+    iterations++;
+  }
+  return resultArray.concat(leftArray.length ? leftArray : rightArray);
 }
