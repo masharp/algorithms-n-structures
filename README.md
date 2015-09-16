fe4d8caef26cc34afc52de1531c25b60c6e1ada8
Michael Sharp <msharp.oh@gmail.com>
2015-09-15 22:00:56 -0400

    updated master-study-log, added symdiff algo, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # useful-algorithms
-Useful algorithms written by me to solve simple and complex problems. Some need polish.
+Useful algorithms written in by me to solve simple and complex problems. Some are written in Java, some in JavaScript.
+Some need polish, some are hacked together out of bits taken from all over the Internet. I need to really study those.
---
+++ SymmetricalDifference.js
@@ -0,0 +1,30 @@
+/* A series of JavaScript methods for finding the symmetricalDifference in two arrays at a time, out of
+  many possible called arguments. Also removes duplicates in individual arrays.
+    - by Michael Sharp
+    - msharp.oh@gmail.com
+    - www.softwareontheshore.com */
+
+function symmetricalDifference() {
+  var args =  Array.prototype.slice.call(arguments); //puts the arguments sent to the function into an array
+
+  /*first filters out duplicates in each argument array, then finds the difference by comparing
+    the difference of the first and second with the second and first array and so on
+    until there are no more arguments */
+  return args.map(uniqueify).reduce(function(first, second) {
+    return difference(first, second).concat(difference(second, first));
+  });
+}
+
+//finds the difference in two arrays by filtering
+function difference(arrayA, arrayB) {
+  return arrayA.filter(function(element) {
+    return arrayB.indexOf(element) == -1;
+  });
+}
+
+//removes duplicate items in the array by filtering out the current index
+function uniqueify(array) {
+  return array.filter(function(element, index) {
+    return array.indexOf(element) == index;
+  });
+}
