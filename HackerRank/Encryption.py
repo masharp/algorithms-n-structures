a4aa0384a69b11649d18e5d9ab2efdcb5a514324
Michael Sharp <msharp.oh@gmail.com>
2016-05-03 21:39:39 -0400

    add python2 solutions to two hackerrank challenges

---
+++ HackerRank/Encryption.py
@@ -0,0 +1,44 @@
+"""
+ * HackerRank Encryption Challenge (JavaScript)
+ * https://www.hackerrank.com/challenges/encryption
+ *
+ * michael@softwareontheshore.com
+"""
+import math
+
+test = ['feedthedog', 'haveaniceday', 'chillout']
+
+def computeArea(length):
+    row = int(math.floor(math.sqrt(length)))
+    column = int(math.ceil(math.sqrt(length)))
+    total = column * row
+    area = { 'row': row, 'column': column, 'total': total }
+    return area
+
+def encrypt(text):
+    sentence = list(text)
+    length = len(text)
+    area = computeArea(length)
+    grid = []
+    encrypted = ''
+
+    for x in range(0, area['total'] + 1, area['column']):
+        segment = sentence[x : x + area['column']]
+        grid.append(segment)
+
+    for y in range(0, area['column'], 1):
+        code = ''
+
+        for a in grid:
+            try:
+                if a[y]:
+                    code += a[y]
+            except IndexError:
+                pass
+
+        encrypted += code + ' '
+
+    return encrypted
+
+result = encrypt(test[0])
+print result
--- HackerRank/LisasWorkbook.py
+++ HackerRank/LisasWorkbook.py
@@ -4,3 +4,40 @@
  *
  * michael@softwareontheshore.com
  """
+testLineA = '5 3'
+testLineB = '4 2 6 1 10'
+
+def findSpecial(lineA, lineB):
+    aArray = map(int, lineA.split(' '))
+    bArray = map(int, lineB.split(' '))
+
+    pageMax = aArray[1]
+    chapterProblems = bArray
+    workbook = []
+    pages = 0
+    specials = 0
+
+    for x in chapterProblems:
+        page = []
+
+        for y in range(1, x + 1, 1):
+            page.append(y)
+
+            if y % pageMax == 0:
+                workbook.append(page)
+                pages += 1
+                page = []
+
+                if pages in workbook[pages - 1]:
+                    specials += 1
+            elif x == y:
+                workbook.append(page)
+                pages += 1
+
+                if pages in workbook[pages - 1]:
+                    specials += 1
+
+    return specials
+
+result = findSpecial(testLineA, testLineB)
+print result
