29866a7b2b949c4e047429117d995a7b03bdb12c
Michael Sharp <msharp.oh@gmail.com>
2016-05-08 12:04:00 -0400

    added makeitanagram

--- HackerRank/MakeItAnagram.py
+++ HackerRank/MakeItAnagram.py
@@ -5,11 +5,21 @@
 *
 * michael@softwareontheshore.com
 """
+from collections import Counter
 
 testAA = 'cde'
 testAB = 'abc'
 
+# use the Counter package to rapidly count the number of letters in the given strings
 def findAnagramDeletions(sA, sB):
-    
+    countA = Counter(sA)
+    countB = Counter(sB)
+
+    # sum the difference between the two strings
+    diffA = (countA - countB).values()
+    diffB = (countB - countA).values()
+    return sum(diffA + diffB)
+
 
 result = findAnagramDeletions(testAA, testAB)
+print result
