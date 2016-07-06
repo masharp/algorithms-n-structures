495bcc9d76b865f074371c1177894b1085af2d8e
Michael Sharp <msharp.oh@gmail.com>
2016-07-06 13:37:42 -0700

    added code eval reverse words

---
+++ CodeEval/ReverseWords.py
@@ -0,0 +1,16 @@
+# Solution to Code Eval's Reverse Words Challenge, written in python
+# https://www.codeeval.com/open_challenges/8/
+#
+# michael@softwareontheshore.com
+
+import sys
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        if test:
+            s = ''
+
+            for x in reversed(test.strip().split(' ')):
+                s += x + ' '
+
+            print s.strip();
