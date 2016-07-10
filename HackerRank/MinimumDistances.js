22e8884c875610067a3fec0f1ca67ccff1287699
Michael Sharp <msharp.oh@gmail.com>
2016-07-10 14:45:19 -0700

    fixed SaveThePrisoner and edited MinimumDistance

--- HackerRank/MinimumDistances.js
+++ HackerRank/MinimumDistances.js
@@ -4,20 +4,7 @@
  *
  * michael@softwareontheshore.com
  */
- /*
-6
-7 1 3 4 1 7
 
-Sample Output
-
-3
-
-Explanation
-Here, we have two options:
-A1 and A4 are both 1, so D14 = |1 - 4| = 3
-A0 and A5 are both 7, so D05 = |0 - 5| = 5
-answer is min(3, 5) = 3
-*/
 'use strict';
 
 const testA = [7, 1, 3, 4, 1, 7];
@@ -25,18 +12,18 @@ const testB = [7, 7, 6, 0, 0, 0, 0, 8, 9, 6];
 
 function findMinDistance(array) {
   const length = array.length;
-  let min = null;
+  let min = -1;
 
   for (let x = 0; x < length; x++) {
     for (let y = x + 1; y < length; y++) {
       if (array[x] === array[y]) {
         const distance = Math.abs(x - y);
 
-        if (min === null || distance < min) min = distance;
+        if (min < 0 || distance < min) min = distance;
       }
     }
   }
-  
+
   return min;
 }
 
--- HackerRank/SaveThePrisoner.js
+++ HackerRank/SaveThePrisoner.js
@@ -1,39 +1,9 @@
 /**
  * Save the Prisonsers! HackerRank Algorithm Challenge
- * http://
+ * https://www.hackerrank.com/challenges/save-the-prisoner
  *
  * michael@software on the shore
-
-A jail has N prisoners, and each prisoner has a unique id number, S, ranging from 1 to N.
-There are M sweets that must be distributed to the prisoners. The jailer decides the fairest way
-to do this is by sitting the prisoners down in a circle (ordered by ascending S), and then,
-starting with some random S, distribute one candy at a time to each sequentially numbered prisoner until all M
-candies are distributed. For example, if the jailer picks prisoner S=2 , then his distribution order
-would be (2, 3, 4, 5, ...., n - 1 n, 1, 2, 3, 4, ...) until all M sweets are distributed.
-
-But wait—there iss a catch—the very last sweet is poisoned!
- Can you find and print the ID number of the last prisoner to receive a sweet so he can be warned?
-
-Input Format
-
-The first line contains an integer, T, denoting the number of test cases.
-The subsequent T lines each contain space-separated integers:
-N(the number of prisoners), M(the number of sweets), and S(the prisoner ID), respectively.
-
-Sample Input
-
-1
-5 2 1
-
-Sample Output
-
-2
-
-Explanation
-
-There are N = 5 prisoners and M = 2 sweets. Distribution starts at ID number S = 1, so prisoner 1 gets
-the first sweet and prisoner 2 gets the second (last) sweet. Thus, we must warn prisoner  2 about the poison,
-so we print on a new line. */
+ */
 
 'use strict';
 
@@ -45,15 +15,13 @@ function savePrisoner(args) {
   const prisoners = args[0]; // number of prisoners
   const sweets = args[1]; // number of sweets
   const start = args[2]; // random start to candy distribution
-  let prisoner = start; // set current prisner to start
 
-  /* hand out candies until out of candies. reset current prisoner if circle cycles */
-  for (let i = 1; i < sweets; i++) {
-    prisoner++;
-    if (prisoner > prisoners) prisoner = 1;
-  }
+  /* finding the remainder of sweets + start - 1 gives us the position
+     of the last candy. if the remainder is 0, the prisoner will be the last,
+     so we return number of prisoners */
+  const prisoner = (sweets + start) % prisoners - 1;
 
-  return prisoner;
+  return prisoner !== 0 ? prisoner : prisoners;
 }
 
 console.log(savePrisoner(testA));
