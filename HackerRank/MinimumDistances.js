e60b394b50133920f9eb9427a084345e6d87b691
Michael Sharp <msharp.oh@gmail.com>
2016-07-10 14:01:22 -0700

    added two hackerrank solutions

--- HackerRank/MinimumDistances.js
+++ HackerRank/MinimumDistances.js
@@ -1,17 +1,10 @@
-/* Consider an array of N integers, A . The distance between two indices, i and j, is denoted by
-Dij = |i - j| <---- ABSOLUTE VALUE
-
-Given A, find the minimum such that and Ai = Aj and i != j. In other words,
-find the minimum distance between any pair of equal elements in the array.
-If no such value exists, print - 1.
-
-Input Format
-
-The first line contains an integer, N, denoting the size of array A.
-The second line contains N space-separated integers describing the respective elements in array A.
-
-Sample Input
-
+/**
+ * JavaScript Solution to MinimumDistance HackerRank Algorithm Challenge
+ * http://
+ *
+ * michael@softwareontheshore.com
+ */
+ /*
 6
 7 1 3 4 1 7
 
@@ -25,3 +18,26 @@ A1 and A4 are both 1, so D14 = |1 - 4| = 3
 A0 and A5 are both 7, so D05 = |0 - 5| = 5
 answer is min(3, 5) = 3
 */
+'use strict';
+
+const testA = [7, 1, 3, 4, 1, 7];
+const testB = [7, 7, 6, 0, 0, 0, 0, 8, 9, 6];
+
+function findMinDistance(array) {
+  const length = array.length;
+  let min = null;
+
+  for (let x = 0; x < length; x++) {
+    for (let y = x + 1; y < length; y++) {
+      if (array[x] === array[y]) {
+        const distance = Math.abs(x - y);
+
+        if (min === null || distance < min) min = distance;
+      }
+    }
+  }
+  
+  return min;
+}
+
+console.log(findMinDistance(testB));
--- HackerRank/SaveThePrisoner.js
+++ HackerRank/SaveThePrisoner.js
@@ -1,4 +1,10 @@
-/*A jail has N prisoners, and each prisoner has a unique id number, S, ranging from 1 to N.
+/**
+ * Save the Prisonsers! HackerRank Algorithm Challenge
+ * http://
+ *
+ * michael@software on the shore
+
+A jail has N prisoners, and each prisoner has a unique id number, S, ranging from 1 to N.
 There are M sweets that must be distributed to the prisoners. The jailer decides the fairest way
 to do this is by sitting the prisoners down in a circle (ordered by ascending S), and then,
 starting with some random S, distribute one candy at a time to each sequentially numbered prisoner until all M
@@ -28,3 +34,26 @@ Explanation
 There are N = 5 prisoners and M = 2 sweets. Distribution starts at ID number S = 1, so prisoner 1 gets
 the first sweet and prisoner 2 gets the second (last) sweet. Thus, we must warn prisoner  2 about the poison,
 so we print on a new line. */
+
+'use strict';
+
+const testA = [5, 2, 1];
+const testB = [10, 10, 5];
+
+function savePrisoner(args) {
+  /* parse args */
+  const prisoners = args[0]; // number of prisoners
+  const sweets = args[1]; // number of sweets
+  const start = args[2]; // random start to candy distribution
+  let prisoner = start; // set current prisner to start
+
+  /* hand out candies until out of candies. reset current prisoner if circle cycles */
+  for (let i = 1; i < sweets; i++) {
+    prisoner++;
+    if (prisoner > prisoners) prisoner = 1;
+  }
+
+  return prisoner;
+}
+
+console.log(savePrisoner(testA));
