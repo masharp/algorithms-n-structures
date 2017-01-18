2d69f1a288732a0a2cfd8bb4cde3a5fcc3b3a18e
Michael Sharp <msharp.oh@gmail.com>
2017-01-18 14:04:38 -0800

    jaro winkler bit

--- computer-science/natural-language-processing/distance/jaro-winkler.js
+++ computer-science/natural-language-processing/distance/jaro-winkler.js
@@ -7,12 +7,17 @@
  * computes the Jaro distance based on http://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
  * compares two strings by finding matching characters and transpositions.
  * a transposition is the number of matching, but different sequence order, characters divided by 2
+ * time and space complexity should be O(|s1| + |s2|)
+ *
+ *
+ *
+ *
  * @param(string) first string to compare
  * @param(string) second string to compare
  * @return(number) jaro distance (between 0 and 1)
  */
 function jaroDistance(stringA, stringB) {
-	if (typeof stringA != 'string' || stringA.length == 0 || typeof stringB != 'string' || stringA.length == 0) { return 0; }
+	if (typeof stringA != 'string' || stringA.length == 0 || typeof stringB != 'string' || stringA.length == 0) { return 0.0; }
 
 	/* compute the range (i +- range) for a valid matching character. this means the character in
 	   stringA must be the same and no farther in the sequence of stringB
@@ -22,15 +27,16 @@ function jaroDistance(stringA, stringB) {
 	var lenB = stringB.length;
 	var maxLen = Math.max(lenA, lenB);
 
+	var sieve = { a: new Array(lenA), b: new Array(lenB) };
 	var matchingRange = (Math.floor(maxLen / 2) - 1);
 	var matching = 0; // sequential character matches
 	var transpositions = 0; // non-sequential character matches within the matching range
-	var jaro = 0;
+	var jaro = 0.0;
 
 	stringA = stringA.toLowerCase();
 	stringB = stringB.toLowerCase();
 
-	if (stringA == stringB) { return 1; }
+	if (stringA == stringB) { return 1.0; }
 
 	for (var x = 0; x < lenA; x++) {
 		var cA = stringA[x];
@@ -38,30 +44,36 @@ function jaroDistance(stringA, stringB) {
 
 		if (cA == cB) { matching++; } // sequential character match
 		else {
+			var found = false;
 
 			/* check for transpositions within the ceiling range */
-			for (var i = x; i < i + matchingRange && i < lenB - 1; i++) {
+			for (var i = x; i < i + matchingRange && i < lenB && found == false; i++) {
 				var tCeil= stringB[i];
 
-				if (tCeil == cA) { transpositions++; }
+				if (tCeil == cA) { matching++; transpositions++; found = true; }
 			}
 
 			/* check for transpositions within the floor range */
-			for (var j = x; j > j - matchingRange && j > -1; j--) {
+			for (var j = x; j > j - matchingRange && j > -1 && found == false; j--) {
 				var tFloor = stringB[j];
 
-				if (tFloor == cA) { transpositions++; }
+				if (tFloor == cA) { matching++; transpositions++; found = true; }
 			}
 		}
 	}
 
-	//console.log(stringA, stringB);
-	//console.log('MATCHING RANGE: ' + matchingRange);
-	//console.log(matching, transpositions);
+	console.log(transpositions)
+	transpositions = transpositions / 2.0;
+
+	console.log(stringA, stringB);
+	console.log('MATCHING RANGE: ' + matchingRange);
+	console.log(matching, transpositions);
 
 	if (matching != 0) {
-		jaro =
+		jaro = ((matching / lenA ) + (matching / lenB) + ((matching - transpositions) / matching)) / 3;
 	}
+
+	console.log('JARO: ' + jaro);
 
 	return jaro;
 }
