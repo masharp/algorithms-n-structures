b2bc1afc366049d97d0078cfcaa72c3ae268dd27
Michael Sharp <msharp.oh@gmail.com>
2017-07-04 12:28:56 -0700

    worked on bst

--- computer-science/data-structures/binary-search-trees/BST.js
+++ computer-science/data-structures/binary-search-trees/BST.js
@@ -2,126 +2,221 @@
  * A Binary Search Tree implemented from scratch in javaScript
  */
 
-function Leaf(value) {
-  this.data = value;
-  this.left = null;
-  this.right = null;
+'use strict';
+
+function Node(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
 }
 
 function BST() {
-  this.root = null;
-  this.size = 0;
+    this.root = null;
+    this.size = 0;
 }
 
 /* BST recursive search */
-BST.prototype.recursiveSearch = function recursiveSearch(value, leaf) {
-  if (!leaf || leaf.data === value) { // base case -> end of tree or target value
-    return leaf;
-  } else if (value < leaf.data) { // follow left child
-    return this.recursiveSearch(value, leaf.left);
-  } else { // follow right child
-    return this.recursiveSearch(value, leaf.right);
-  }
-
-}
-/* BST iterative search */
-BST.prototype.iterativeSearch = function iterativeSearch(value, leaf) {
-  var currentLeaf = leaf;
-
-  while (currentLeaf) {
-    if (key === currentLeaf.data) { // base case is the value is present in leaf
-      return currentLeaf;
-    } else if (value < currentLeaf.left.data) {
-      currentLeaf = currentLeaf.left;
-    } else {
-      currentLeaf = currentLeaf.right;
+BST.prototype.recursiveSearch = function recursiveSearch(value, node) {
+    if (!node || node.data === value) { // base case -> end of tree or target value
+        return node;
+    } else if (value < node.data) { // follow left child
+        return this.recursiveSearch(value, node.left);
+    } else { // follow right child
+        return this.recursiveSearch(value, node.right);
     }
-  }
+}
 
-  return null; // value not present
+/* BST iterative search */
+BST.prototype.iterativeSearch = function iterativeSearch(value, node) {
+    let currentNode = node;
+
+    while (currentNode) {
+        if (key === currentNode.data) { // base case is the value is present in node
+            return currentNode;
+        } else if (value < currentNode.left.data) {
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+
+    return null; // value not present
+}
+
+/* BST iterative insertion */
+BST.prototype.add = function add(value) {
+    let node = new Node(value);
+    this.size++;
+
+    if (!this.root) { // empty tree
+        this.root = node;
+    } else {
+        let currentNode = this.root;
+
+        /* compares to left, right then proceeds down the tree */
+        while (currentNode) {
+            if (node.data < currentNode.data) {
+                if (!currentNode.left) {
+                    currentNode.left = node;
+                    return true;
+                } else {
+                    currentNode = currentNode.left;
+                }
+            } else if (node.data > currentNode.data) {
+                if (!currentNode.right) {
+                    currentNode.right = node;
+                    return true;
+                } else {
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+}
+
+BST.prototype.remove = function remove(value) {
+    let found = false;
+    let parent = null;
+    let current = this.root;
+    let childCount = 0;
+    let replacement = null;
+    let replacementParent = null;
+
+    while (!found && current) {
+        if (value < current.value) {
+            parent = current;
+            current = current.left;
+
+        } else if (value > current.value) {
+            parent = current;
+            current = current.left;
+
+        } else {
+            found = true;
+        }
+    }
+
+    if (found) {
+        childCount = (current.left !== null ? 1 : 0) + (current.right !== null ? 1 : 0);
+
+        if (current === this.root) {
+            switch (childCount) {
+                case 0:
+                    this.root = null;
+                    break;
+                case 1:
+                    this.root = (current.right === null ? current.left : current.right);
+                    break;
+                case 2:
+                    replacement = this.root.left;
+
+                    while (replacement.right !== null) {
+                        replacementParent = replacement;
+                        replacement = replacement.right;
+                    }
+
+                    if (replacementParent !== null) {
+                        replacementParent.right = replacement.left;
+
+                        replacement.right = this.root.right;
+                        replacement.left  this.root.left;
+                    } else {
+                        replacement.right == this.root.right;
+                    }
+
+                    this.root = replacement;
+                    break;
+            }
+        } else {
+        switch (childCount) {
+                case 0:
+                    if (current.value < parent.value) {
+                        parent.left = null;
+
+                    } else {
+                        parent.right = null;
+                    }
+                    break;
+
+                case 1:
+                    if (current.value < parent.value) {
+                        parent.left = (current.left === null ? current.right : current.left);
+
+                    } else {
+                        parent.right = (current.left === null ? current.right : current.left);
+                    }
+
+                    break;
+               case 2:
+                   replacement = current.left;
+                   replacementParent = current;
+
+                   while (replacement.right !== null){
+                       replacementParent = replacement;
+                       replacement = replacement.right;
+                   }
+
+                   replacementParent.right = replacement.left;
+                   replacement.right = current.right;
+                   replacement.left = current.left;
+
+                   if (current.value < parent.value) {
+                       parent.left = replacement;
+                   } else {
+                       parent.right = replacement;
+                   }
+           }
+        }
+    }
 }
 
 /* helper method that checks if value is present in BST */
 BST.prototype.contains = function contains(value) {
-  var result = this.iterativeSearch(value, this.root);
+    var result = this.iterativeSearch(value, this.root);
 
-  if (result) return true;
-  else false;
+    if (result) return true;
+    else false;
 }
 
+BST.prototype.traverse = function traverse(process) {
+    function inOrder(node) {
+        if (node) {
+            if (node.left !== null) {
+                inOrder(node.left);
+            }
 
-/* BST iterative insertion */
-BST.prototype.add = function add(value) {
-  var node = new Leaf(value);
-  this.size++;
+            process.call(this, node);
 
-  if (!this.root) { // empty tree
-    this.root = node;
-  } else {
-    var currentLeaf = this.root;
-
-    // compares to left, right then proceeds down the tree
-    while (currentLeaf) {
-      if (node.data < currentLeaf.data) {
-        if (!currentLeaf.left) {
-          currentLeaf.left = node;
-          return true;
-        } else {
-          currentLeaf = currentLeaf.left;
+            if (node.right !== null) {
+                inOrder(node.right);
+            }
         }
-      } else if (node.data > currentLeaf.data) {
-        if (!currentLeaf.right) {
-          currentLeaf.right = node;
-          return true;
-        } else {
-          currentLeaf = currentLeaf.right;
-        }
-      }
     }
-  }
+
+    inOrder(this.root);
 }
-
-/* BST iterative removal
-BST.prototype.remove = function remove(value) {
-  var currentLeaf = this.root;
-
-  if (!currentLeaf) return null;
-  else {
-    while (currentLeaf.data !== value || currentLeaf) {
-      if (value < currentLeaf.left.data && currentLeaf.left) {
-        currentLeaf = currentLeaf.left;
-      } else if (value > currentLeaf.right.data && currentLeaf.right) {
-        currentLeaf = currentLeaf.right;
-      }
-    }
-  }
-}*/
 
 BST.prototype.size = function size() {
-  return this.size;
+    let length = 0;
+
+    this.traverse(node => length++);
+
+    return length;
 }
 
-/* recursive inorder (left first) tree traversal in order to build an array */
 BST.prototype.toArray = function toArray() {
-  var result = [];
+    const result = [];
 
-  function traverse(leaf) {
-    if (!leaf) return;
+    this.traverse(node => result.push(node.value));
 
-    traverse(leaf.left);
-    result[result.length] = leaf.data;
-    traverse(left.right);
-  }
-
-  traverse(this.root);
-  return result;
+    return result;
 }
 
 BST.prototype.toString = function toString() {
-  return this.toArray().toString();
+    return this.toArray().toString();
 }
 
-var tree = new BST();
+const tree = new BST();
 tree.add(1313);
 tree.add(12);
 tree.add(245);
--- computer-science/data-structures/binary-search-trees/BST.py
+++
@@ -1,6 +0,0 @@
-"""
- * A Binary Search Tree implemented from scratch in Python
-"""
-
-class BST(object):
-    def __init__(self)
