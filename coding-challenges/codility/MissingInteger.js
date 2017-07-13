1cf277b28968a1151e8cb1c49d37509907730038
Michael Sharp <msharp.oh@gmail.com>
2017-07-13 09:21:56 -0700

    updated misisngint

--- coding-challenges/codility/MissingInteger.js
+++ coding-challenges/codility/MissingInteger.js
@@ -1,29 +1,9 @@
-/*
-
-Write a function:
-
-function solution(A);
-that, given a non-empty zero-indexed array A of N integers, returns the minimal positive integer (greater than 0) that does not occur in A.
-
-For example, given:
-
-  A[0] = 1
-  A[1] = 3
-  A[2] = 6
-  A[3] = 4
-  A[4] = 1
-  A[5] = 2
-the function should return 5.
-
-Assume that:
-
-N is an integer within the range [1..100,000];
-each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
-Complexity:
-
-expected worst-case time complexity is O(N);
-expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
-Elements of input arrays can be modified.
+/**
+ * that, given a non-empty zero-indexed array A of N integers,
+ * returns the minimal positive integer (greater than 0) that does not occur in A.
+ *
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 */
 
 const testA = [1, 3, 6, 4, 1, 2]; // 5
@@ -37,15 +17,16 @@ function solution(A) {
     let num = 1;
 
     for (let x = 0; x < N; x++) {
-        if (checked[S[x]]) continue;
+        if (checked[S[x]]) continue; // ignore duplicate integers in sorted array
 
         if (S[x] > 0 && S[x] != num) {
             return num;
+
         } else if (S[x] > 0 && S[x] == num) {
             num++;
         }
 
-        checked[S[x]] = 1;
+        checked[S[x]] = true;
     }
 
     return num;
