5c32480a79ea50f9c64a4127ac205cd4c5b0c611
Michael Sharp <msharp.oh@gmail.com>
2017-07-13 20:12:29 -0700

    added solution to codility problems.

--- coding-challenges/codility/PermCheck.js
+++ coding-challenges/codility/PermCheck.js
@@ -1,53 +1,9 @@
 /**
+ * A non-empty zero-indexed array A consisting of N integers is given.
+ * A permutation is a sequence containing each element from 1 to N once, and only once.
  *
- A non-empty zero-indexed array A consisting of N integers is given.
-
- A permutation is a sequence containing each element from 1 to N once, and only once.
-
- For example, array A such that:
-
-     A[0] = 4
-     A[1] = 1
-     A[2] = 3
-     A[3] = 2
- is a permutation, but array A such that:
-
-     A[0] = 4
-     A[1] = 1
-     A[2] = 3
- is not a permutation, because value 2 is missing.
-
- The goal is to check whether array A is a permutation.
-
- Write a function:
-
- function solution(A);
- that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
-
- For example, given array A such that:
-
-     A[0] = 4
-     A[1] = 1
-     A[2] = 3
-     A[3] = 2
- the function should return 1.
-
- Given array A such that:
-
-     A[0] = 4
-     A[1] = 1
-     A[2] = 3
- the function should return 0.
-
- Assume that:
-
- N is an integer within the range [1..100,000];
- each element of array A is an integer within the range [1..1,000,000,000].
- Complexity:
-
- expected worst-case time complexity is O(N);
- expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
- Elements of input arrays can be modified.
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  */
 
  const testA = [4, 1, 3, 2]; // 1
@@ -55,7 +11,14 @@
  const testC = [5, 5, 1, 2, 3, 4]; // 0
 
 function solution(A) {
+    const N = A.length;
+    A = A.sort((a, b) => a - b);
 
+    for (let x = 0; x < N; x++) {
+        if (A[x] != x + 1) { return 0; }
+    }
+
+    return 1;
 };
 
 console.log(solution(testA));
